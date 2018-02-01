7eeefe042b049c01f6ccaef55029242975cd4dd1
ryoji7583 <ryoji.75830923@gmail.com>
2018-02-01 10:20:25 +0900

    task1-2-1
    値を追加するための汎用DBメソッドの作成

--- src/jp/ac/oit/igakilab/tctr/db/TodoDatabaseCreator.java
+++ src/jp/ac/oit/igakilab/tctr/db/TodoDatabaseCreator.java
@@ -9,13 +9,19 @@ import org.apache.ibatis.session.SqlSessionFactory;
 import jp.ac.oit.igakilab.tctr.todoList.todoManager;
 
 public class TodoDatabaseCreator {
-SqlSessionFactory factory = DBUtility.getSqlSessionFactory();
+	SqlSessionFactory factory = DBUtility.getSqlSessionFactory();
 
-public List<todoManager> todoListen(){
-List <todoManager> todoItem = new ArrayList<>();
-try (SqlSession session = factory.openSession()){
-todoItem = session.selectList("todoListen");
-}
-return todoItem;
-}
+	public List<todoManager> todoListen(){
+		List <todoManager> todoItem = new ArrayList<>();
+		try (SqlSession session = factory.openSession()){
+			todoItem = session.selectList("todoListen");
+		}
+		return todoItem;
+	}
+	public void todoInsert(String mapper,todoManager tm){
+		try (SqlSession session = factory.openSession()){
+			session.insert(mapper,tm);
+			session.commit();
+		}
+	}
 }
