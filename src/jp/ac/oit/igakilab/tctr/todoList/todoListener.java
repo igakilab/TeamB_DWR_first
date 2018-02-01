19acf9ec2a3734d085ecfa1345aa1365ed2e4476
ryoji7583 <ryoji.75830923@gmail.com>
2018-02-01 10:17:07 +0900

    task1-1-4
    実行

--- src/jp/ac/oit/igakilab/tctr/todoList/todoListener.java
+++ src/jp/ac/oit/igakilab/tctr/todoList/todoListener.java
@@ -5,12 +5,18 @@ import java.util.List;
 import jp.ac.oit.igakilab.tctr.db.TodoDatabaseCreator;
 
 public class todoListener {
-public List<todoManager> todoListen(){
-TodoDatabaseCreator mdb = new TodoDatabaseCreator();
-return mdb.todoListen();
-}
+	public List<todoManager> todoListen(){
+		TodoDatabaseCreator mdb = new TodoDatabaseCreator();
+		return mdb.todoListen();
+	}
 
-public static void main(String[] args){
-
-}
+	public static void main(String[] args){
+		todoListener tdm = new todoListener();
+		for(todoManager t:tdm.todoListen()){
+			System.out.println(t.getTitle());
+			System.out.println(t.getId());
+			System.out.println(t.getDate());
+			System.out.println(t.getOverview());
+		}
+	}
 }
