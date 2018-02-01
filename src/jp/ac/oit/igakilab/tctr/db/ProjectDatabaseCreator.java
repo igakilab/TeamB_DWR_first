0d02e0cf6ac7bd211c299bee4d8db042468a4dfb
pata1030 <takahata1030>
2018-02-01 10:12:51 +0900

    task1-1-1 修正　task1-1-2 実装
    1-1-1
    インデントが一部間違っていた
    
    1-1-2
    テーブルProjectと接続するためのDB汎用クラスの作成

---
+++ src/jp/ac/oit/igakilab/tctr/db/ProjectDatabaseCreator.java
@@ -0,0 +1,23 @@
+package jp.ac.oit.igakilab.tctr.db;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+
+import jp.ac.oit.igakilab.tctr.project.projectManager;
+
+public class ProjectDatabaseCreator {
+    SqlSessionFactory factory = DBUtility.getSqlSessionFactory();
+    List<projectManager> selectList = new ArrayList<>();
+
+
+
+    public List<projectManager> projectListen(String mapper){
+        try (SqlSession session = factory.openSession()){
+            selectList = session.selectList(mapper);
+        }
+        return selectList;
+    }
+}
