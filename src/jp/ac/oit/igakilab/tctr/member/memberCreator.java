b2f45ab8f0428f7c8d391127fc0daf75d8a5de3c
wuyou284 <wuyou284@gmail.com>
2018-02-01 10:17:31 +0900

    member_task1-2-2
    memberTasksのtask1-2-2　実際にDBのデータを追加する部分とSQLの作成を行った。

---
+++ src/jp/ac/oit/igakilab/tctr/member/memberCreator.java
@@ -0,0 +1,25 @@
+package jp.ac.oit.igakilab.tctr.member;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.commons.lang3.RandomStringUtils;
+
+import jp.ac.oit.igakilab.tctr.db.MemberDatabaseCreator;
+
+public class memberCreator {
+MemberDatabaseCreator mdc = new MemberDatabaseCreator();
+List<memberManager> mml = new ArrayList<>();
+memberManager mm = new memberManager();
+public void memberInsert(String name,String org,String position,int age){
+mm.setName(name);
+mm.setOrg(org);
+mm.setPosition(position);
+mm.setAge(age);
+mm.setId(RandomStringUtils.randomAlphabetic(1)+RandomStringUtils.randomAlphanumeric(5));
+mdc.memberInsert("memberInsert", mm);
+}
+
+public static void main(String[] args){
+}
+}
