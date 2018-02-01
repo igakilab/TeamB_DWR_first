8eb6b17a3c70582ce31b6414b64ffd81a5c1a257
pata1030 <takahata1030>
2018-02-01 10:59:48 +0900

    task 2-2-3 実装
    javascriptの編集

---
+++ WebContent/js/project/projectListener.js
@@ -0,0 +1,19 @@
+/*projectListを生成するためのjavaScript*/
+projectListener.projectListen({
+callback:function(data){
+for(var i of data){
+
+$('#selectProjectName').append('<option value="'+i.id+'">'+i.name+'</option>')
+$('#projectList').append('<li><div class="projectName">プロジェクト名:'+i.name+'</div><div class ="projectState">状態:'+i.state+'</div><input type="button" value="状態変更" onclick= projectChange("'+i.id+'")></li>')
+}
+
+}
+});
+
+projectListener.ownerCandidate({
+callback:function(own){
+for(var j of own){
+$('#selectOwner').append('<option value="'+j.owner+'">'+j.owner+'</option>')
+}
+}
+});
