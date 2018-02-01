6587365da368b2f9f1f2722655e2d41b2e0e4ddc
pata1030 <takahata1030>
2018-02-01 10:19:34 +0900

    1-1-4 実装

--- src/jp/ac/oit/igakilab/tctr/project/projectListener.java
+++ src/jp/ac/oit/igakilab/tctr/project/projectListener.java
@@ -11,6 +11,13 @@ return pdc.projectListen("listenProject");
 }
 
 public static void main(String[] args){
-
+projectListener pl = new projectListener();
+for(projectManager pm:pl.projectListen()){
+System.out.println("ID:"+pm.getId());
+System.out.println("Name:"+pm.getName());
+System.out.println("Owner:"+pm.getOwner());
+System.out.println("Overview:"+pm.getOverview());
+System.out.println("State:"+pm.getState());
+}
 }
 }
